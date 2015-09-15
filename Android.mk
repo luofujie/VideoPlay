cc34a645cb4201ef424dd61aafe033d6ad0a0ee9
luofujie <luofujie@126.com>
2015-09-15 18:23:48 +0800

    Test

---
+++ Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_SDK_VERSION := current
+LOCAL_PACKAGE_NAME := bright-yinjk
+LOCAL_CERTIFICATE := platform
+include $(BUILD_PACKAGE)
+include $(call all-makefiles-under,$(LOCAL_PATH))
